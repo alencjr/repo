fa211959e9b88480476581431db7747ea7fb4ed3
cdealenc <claudiodealencar@Claudios-MacBook-Pro.local>
2022-01-08 19:54:08 +0100

    input added

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
-class Main{
-	public static void main(String args[]){
-		System.out.println("Hello");
-	}
+import java.util.Scanner;
+
+class Main{ 
+	public static void main(String args[]){ 
+		System.out.print("hello world!");
+		Scanner in = new Scanner(System.in);
+		String name = in.next();
+		System.out.println(name);		
+	} 
 }
